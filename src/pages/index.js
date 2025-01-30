affa509c011e9a46009214d057f458620cb1fc4d
ka5perV <kasper-virta@hotmail.com>
2025-01-30 11:14:43 +0200

    router

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+// Filename - pages/index.js
+
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home page yeeehaawww</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/products.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const Products = () => {
+    return (
+        <div>
+            <h1>
+                About page yeeehaawww
+            </h1>
+        </div>
+    );
+};
+
+export default Products;
