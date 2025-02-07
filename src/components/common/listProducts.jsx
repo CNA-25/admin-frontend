0575f057c2d7cb89f023b0dae0c94133123a2379
ka5perV <kasper-virta@hotmail.com>
2025-02-07 12:38:06 +0200

    Prok update  - list procuts test

---
+++ src/components/common/listProducts.jsx
@@ -0,0 +1,66 @@
+import "../../style/List.css";
+import porkImage from "../../assets/pork.jpg";
+
+const products = [
+    {
+        "sku": "123-ABC",
+        "name": "Pale Ale",
+        "price": 59.99,
+        "description": "En fruktig och frisk pale ale. En fruktig och frisk pale ale. En fruktig och frisk pale ale. En fruktig och frisk pale ale. En fruktig och frisk pale ale.",
+        "image": "../assets/images/pork.jpg",
+        "created_at": "2024-01-01T20:54:00Z",
+        "updated_at": "2024-01-01T20:54:00Z"
+    },   {
+        "sku": "123-ABC",
+        "name": "kakirumppa",
+        "price": 59.99,
+        "description": "En fruktig och frisk pale ale.",
+        "image": "path-to-image",
+        "created_at": "2024-01-01T20:54:00Z",
+        "updated_at": "2024-01-01T20:54:00Z"
+    },   {
+        "sku": "123-ABC",
+        "name": "kakirumppa",
+        "price": 59.99,
+        "description": "En fruktig och frisk pale ale.",
+        "image": "path-to-image",
+        "created_at": "2024-01-01T20:54:00Z",
+        "updated_at": "2024-01-01T20:54:00Z"
+    }
+];
+
+
+
+export default function List() {
+    return (
+        <div className="product-list">
+            {products.map((product, index) => (
+                <div key={index} className="product-box">
+                    <div className="product-info">
+                        <div className="product-detail">
+                            <strong>Namn:</strong> {product.name}
+                        </div>
+                        <div className="product-detail">
+                            <strong>Price:</strong> {product.price} $
+                        </div>
+                        <div className="product-detail">
+                            <strong>SKU:</strong> {product.sku}
+                        </div>
+                        <div className="product-detail">
+                            <strong>Bio:</strong> {product.description}
+                        </div>
+                        <div className="product-detail">
+                            <strong>Created:</strong> {new Date(product.created_at).toLocaleString()}
+                        </div>
+                        <div className="product-detail">
+                            <strong>Updated:</strong> {new Date(product.updated_at).toLocaleString()}
+                        </div>
+                    </div>
+                    <div className="product-image">
+                        <img src={porkImage} alt="img src" /> <br /><br />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,24 @@
 // Products page
 import React from "react";
+import List from "../components/common/listProducts";
+import "../style/List.css";
+import "../style/SearchBar.css";
 
 const Products = () => {
     return (
         <div>
-            <h1>
+            <h1 className="h1">
                 Products page
             </h1>
+            
+            <div className="search-container">
+            <input type="text" placeholder="Search..." />
+            </div>
+
+
+            <List /> {/* Using the List component here */}
         </div>
+        
     );
 };
 
