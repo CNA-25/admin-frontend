4172c8d664ca26056da1e3668d72f135cd67ab2e
niskanjo <niskanjo@arcada.fi>
2025-02-26 00:59:05 +0200

    Add user API login request

--- src/components/page-specific/LoginForm.jsx
+++ src/components/page-specific/LoginForm.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+
 // Based on GeeksForGeeks tutorial: Basic Registration and Login Form Using React Hook Form
 // https://www.geeksforgeeks.org/react-hook-form-create-basic-reactjs-registration-and-login-form/
 
 // Login form
 function LoginForm() {
-    const {register, handleSubmit, formState: { errors } } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = (data) => {
+  // Define URL for user service API
+  API_URL = process.env('USER_SVC') || 'https://user-service-api-user-service.2.rahtiapp.fi';
+
+  const onSubmit = (data) => {
+    // Send request to user service API
+    try {
+      const resp = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: data.email,
+          password: data.password
+        });
+      });
+      const respData = await resp.json();
+
+      if (resp.ok) {
+        // Save JWT to localStorage
+        localStorage.setItem('user_token', respData.access_token);
+
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+
+      /*
       const userData = JSON.parse(localStorage.getItem(data.email));
       if (userData) {
         if (userData.password === data.password) {
@@ -20,6 +46,7 @@ function LoginForm() {
         console.log("Email or password is incorrect.")
       }
     };
+    */
 
     // Form UI
     return (
@@ -33,6 +60,6 @@ function LoginForm() {
         </form>
       </>
     );
-}
+  }
 
-export default LoginForm;
+  export default LoginForm;
