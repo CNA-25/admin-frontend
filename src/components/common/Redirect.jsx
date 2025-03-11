159a3e05f6810db40c0f2dc511bf3d116e41639f
niskanjo <niskanjo@arcada.fi>
2025-03-11 21:57:23 +0200

    Add logout functionality, tweak redirect

--- src/components/common/Redirect.jsx
+++ src/components/common/Redirect.jsx
@@ -1,5 +1,6 @@
 // Global redirect component
 import React from "react";
+import { jwtDecode } from "jwt-decode";
 import { Navigate } from "react-router-dom";
 
 const Redirect = ({ children }) => {
@@ -8,6 +9,22 @@ const Redirect = ({ children }) => {
     // Check if token exists in localStorage
     if (!token) {
         return <Navigate to="/login" replace />;
+    } else {
+        try {
+            // Decode JWT data
+            const decodedToken = jwtDecode(token);
+            // Check if role is admin or user
+            if (decodedToken.role !== "admin") {
+                console.log(`Authorization error: Insufficient access rights`);
+                // Redirect to login page
+                return <Navigate to="/login" replace />
+            }
+
+            return <Navigate to="/" replace />
+        } catch (error) {
+            console.error("Error: ", error.message);
+        }
+
     }
 
     return children;
